cc0aef1f1bf7d4033010a252e77e9b7e9f6b62ee
freexperia <jerpelea@gmail.com>
2012-05-30 09:30:10 +0300

    audio/msm7x30: fix for SEMC devices with stereo hw speaker
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I2a326b13101bfe589eebe46012559633d8b361df

--- msm7x30/Android.mk
+++ msm7x30/Android.mk
@@ -4,6 +4,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(BOARD_USES_STEREO_HW_SPEAKER),true)
+    LOCAL_CFLAGS += -DWITH_STEREO_HW_SPEAKER
+endif
+
 LOCAL_SRC_FILES := \
     AudioHardware.cpp \
     audio_hw_hal.cpp
