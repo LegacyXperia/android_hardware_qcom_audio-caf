6a80daf9e099799ff3ee4974cb5772fd7ae98736
Michael Bestas <mikeioannina@gmail.com>
2013-11-17 18:27:55 +0200

    legacy/msm7x30: Use CM standard qcom naming

--- legacy/Android.mk
+++ legacy/Android.mk
@@ -12,6 +12,6 @@ ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
     include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
 endif
 
-ifeq ($(call is-board-platform,msm7630_surf),true)
-    include $(AUDIO_HW_ROOT)/msm7630/Android.mk
+ifeq ($(call is-board-platform,msm7x30),true)
+    include $(AUDIO_HW_ROOT)/msm7x30/Android.mk
 endif
